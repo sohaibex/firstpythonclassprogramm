4c0d0cc6b0a1bea210a7baa9979d566a7fccf5f1
SOHAIB EL MEDIOUNI <sohaib.elmediouni23@gmail.com>
2021-03-13 10:11:36 +0100

    arrays

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+# indetation is required
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Sohaib')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # print('Enter your name:')
+    # x = input()
+    # print('Hello, ' + x)
+
+    x=3
+    y=2.5
+    print(x / y)
+    print(x//y)
+    print(x * y)
+    print(x ** y)
+
+    film=["limitless","war horse","the man who knows the infinity"];
+
+
+
